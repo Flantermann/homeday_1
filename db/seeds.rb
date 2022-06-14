30e87c0aa0cec96119aa89e9060f1472cc062721
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-06-14 16:23:18 +0200

    seed database with realtors from json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 # seeding the databse with realtors from realtors.json
 
 require 'json'
+filepath = "lib/seeds/realtors.json"
+serialized_realtors = File.read(filepath)
+realtors = JSON.parse(serialized_realtors)
 
 puts "creating realtors..."
 
-path = Rails.root.join('lib', 'seeds', 'realtors.json')
-realtors = JSON.parse(File.read(path))
-
-realtors["realtors"].each do |realtor|
-    Realtor.create(realtor)
+realtors.each do |realtor|
+    Realtor.create(name: realtor["name"], city: realtor["city"], lat: realtor["lat"], lng: realtor["lng"] )
 end
 
 puts "#{Realtor.count} rows were created"
