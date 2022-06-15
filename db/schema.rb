b6db5a911cfe5184da6826cccb5b5bb56db7b30b
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-06-15 12:30:50 +0200

    add gem activerecord-postgres-earthdistance and bundle install

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_145638) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_15_103010) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "cube"
+  enable_extension "earthdistance"
   enable_extension "plpgsql"
 
   create_table "appointments", force: :cascade do |t|
