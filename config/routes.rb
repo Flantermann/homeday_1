2dbe8a471521078a8775343d7ca3baf1f54f9528
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-06-15 13:01:16 +0200

    add routes for past and future appointments

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :appointments, only: :create
 
-  resources :realtors, only: :show do
-    resources :appointments, only: :index do
-      # member do
-      #   get „past“, to: „appointments#past“
-      #   get „future“, to: „appointments#future“
-      # end
-    end
-  end
+  get '/realtors/:id/appointments/past', to: 'appointments#past'
+  get '/realtors/:id/appointments/future', to: 'appointments#future'
 end
 
 # VARIANTE:
@@ -26,3 +16,12 @@ end
 #       end
 #     end
 #   end
+
+  # resources :realtors, only: :show do
+  #   resources :appointments, only: :index do
+  #     # member do
+  #     #   get „past“, to: „appointments#past“
+  #     #   get „future“, to: „appointments#future“
+  #     # end
+  #   end
+  # end
