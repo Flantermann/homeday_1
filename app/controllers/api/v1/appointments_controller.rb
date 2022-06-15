26db4c10d32d97080ca7b6cff0d19febd05f3198
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-06-15 23:23:13 +0200

    clean up appointment controller

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -15,12 +15,13 @@ class Api::V1::AppointmentsController < ApplicationController
         # that means no realtor is working in the area
         # give meaningful error message
       else
-        # if realtor is found, then check for their availability (how?)
-        # if realtor has appointment at appointment.time or in the 30 minutes before said time, they are not available
-        # is no realtor is found: provide error message
-        appointment.realtor = realtor
+        # still missing:
+          # if realtor is found, then check for their availability (how?)
+          # if realtor has appointment at appointment.time or in the 30 minutes before said time, they are not available
+          # is no realtor is found: provide error message
+        appointment.realtor = realtor #should this be an update of saved appointment instance?
       end
-      seller = Seller.create(name: appointment.seller.name, phone: appointment.seller.phone)save seller
+      seller = Seller.create(name: appointment.seller.name, phone: appointment.seller.phone)
       appointment.seller = seller
       render json: appointment, status: :created
     else
@@ -29,15 +30,15 @@ class Api::V1::AppointmentsController < ApplicationController
   end
 
   def past
-    @past_appointments = realtor.appointments.where(time: (Time.now.months_ago(1).beginning_of_month)..(Time.now.months_ago(1).end_of_month)).order(time: :desc)
+    past_appointments = realtor.appointments.where(time: (Time.now.months_ago(1).beginning_of_month)..(Time.now.months_ago(1).end_of_month)).order(time: :desc)
     # better way to handle output?
-    render json: @past_appointments
+    render json: past_appointments
   end
 
   def future
-    @future_appointments = realtor.appointments.where(time: (Time.now.beginning_of_month + 1.month)..(Time.now.beginning_of_month + 2.month - 1.day)).order(:time)
+    future_appointments = realtor.appointments.where(time: (Time.now.beginning_of_month + 1.month)..(Time.now.beginning_of_month + 2.month - 1.day)).order(:time)
     # better way to handle output?
-    render json: @future_appointments
+    render json: future_appointments
   end
 
   private
